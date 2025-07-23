522cba50193fd87d865b4e96d0a588f67071721d
Amauri Franco <programador.amauri@gmail.com>
2025-07-23 11:34:21 -0300

    Fix treesitter queries and stylua typo

--- lua/neotest-pest/init.lua
+++ lua/neotest-pest/init.lua
@@ -1,7 +1,7 @@
-local lib = require('neotest.lib')
-local logger = require('neotest.logging')
-local utils = require('neotest-pest.utils')
-local config = require('neotest-pest.config')
+local lib = require("neotest.lib")
+local logger = require("neotest.logging")
+local utils = require("neotest-pest.utils")
+local config = require("neotest-pest.config")
 local debug = logger.debug
 
 ---@class neotest.Adapter
@@ -15,35 +15,35 @@ local NeotestAdapter = { name = "neotest-pest" }
 ---@param dir string @Directory to treat as cwd
 ---@return string | nil @Absolute root dir of test suite
 function NeotestAdapter.root(dir)
-    local result = nil
+  local result = nil
 
-    debug("Finding root...")
+  debug("Finding root...")
 
-    for _, root_ignore_file in ipairs(config("root_ignore_files")) do
-        debug("Checking root ignore file", root_ignore_file)
+  for _, root_ignore_file in ipairs(config("root_ignore_files")) do
+    debug("Checking root ignore file", root_ignore_file)
 
-        result = lib.files.match_root_pattern(root_ignore_file)(dir)
+    result = lib.files.match_root_pattern(root_ignore_file)(dir)
 
-        if result then
-            debug("Ignoring root because file", root_ignore_file)
-            return nil
-        end
+    if result then
+      debug("Ignoring root because file", root_ignore_file)
+      return nil
     end
+  end
 
-    for _, root_file in ipairs(config("root_files")) do
-        debug("Checking root file", root_file)
+  for _, root_file in ipairs(config("root_files")) do
+    debug("Checking root file", root_file)
 
-        result = lib.files.match_root_pattern(root_file)(dir)
+    result = lib.files.match_root_pattern(root_file)(dir)
 
-        if result then
-            debug("Found root", result)
-            break
-        end
+    if result then
+      debug("Found root", result)
+      break
     end
+  end
 
-    debug("Root not found")
+  debug("Root not found")
 
-    return result
+  return result
 end
 
 ---Filter directories when searching for test files
@@ -53,107 +53,111 @@ end
 ---@param root string Root directory of project
 ---@return boolean True when matching
 function NeotestAdapter.filter_dir(name, rel_path, root)
-    for _, filter_dir in ipairs(config("ignore_dirs")) do
-        if name == filter_dir then return false end
+  for _, filter_dir in ipairs(config("ignore_dirs")) do
+    if name == filter_dir then
+      return false
     end
+  end
 
-    return true
+  return true
 end
 
 ---@async
 ---@param file_path string
 ---@return boolean
 function NeotestAdapter.is_test_file(file_path)
-    for _, suffix in ipairs(config("test_file_suffixes")) do
-        if vim.endswith(file_path, suffix) then return true end
+  for _, suffix in ipairs(config("test_file_suffixes")) do
+    if vim.endswith(file_path, suffix) then
+      return true
     end
+  end
 
-    return false
+  return false
 end
 
 function NeotestAdapter.discover_positions(path)
-    local query = [[
-        ((expression_statement
-            (member_call_expression
-                name: (name) @member_name (#eq? @member_name "group")
-                arguments: (arguments . (argument (string (string_content) @namespace.name)))
-            ) @member
-        )) @namespace.definition
-
-        ((function_call_expression
+  local query = [[
+      (expression_statement
+        (member_call_expression
+          object: (function_call_expression
             function: (name) @func_name (#match? @func_name "^(test|it)$")
             arguments: (arguments . (argument (_ (string_content) @test.name)))
-        )) @test.definition
+          ) @test.definition
+        )
+      ) @test.definition
 
-        ((expression_statement
-            (member_call_expression
-                object: (#eq? @test.definition)
-                name: (name) @member_call_name (#match? @member_call_name "^(with)$")
-                arguments: (arguments . (argument (array_creation_expression (array_element_initializer (array_creation_expression (array_element_initializer (_) @test.parameter .) )))))
-            )
-        ))
-    ]]
+      (function_call_expression
+        function: (name) @func_name (#match? @func_name "^(test|it)$")
+        arguments: (arguments . (argument (_ (string_content) @test.name)))
+      ) @test.definition
 
-    return lib.treesitter.parse_positions(path, query, {
-        position_id = "require('neotest-pest.utils').make_test_id",
-    })
+      (expression_statement
+        (member_call_expression
+          name: (name) @member_name (#eq? @member_name "group")
+          arguments: (arguments . (argument (string (string_content) @namespace.name)))
+        ) @namespace.definition
+      )
+  ]]
+
+  return lib.treesitter.parse_positions(path, query, {
+    position_id = "require('neotest-pest.utils').make_test_id",
+  })
 end
 
 ---@param args neotest.RunArgs
 ---@return neotest.RunSpec | nil
 function NeotestAdapter.build_spec(args)
-    local position = args.tree:data()
-    local results_path = config('results_path')
+  local position = args.tree:data()
+  local results_path = config("results_path")
 
-    debug("Building spec for:", position)
-    debug("Results path:", results_path)
+  debug("Building spec for:", position)
+  debug("Results path:", results_path)
 
-    local path = position.path;
+  local path = position.path
 
-    if config('sail_enabled') then
-        debug("Sail enabled, adjusting path")
-        path = config('sail_project_path') .. string.sub(position.path, string.len(vim.loop.cwd() or "") + 1)
-    end
+  if config("sail_enabled") then
+    debug("Sail enabled, adjusting path")
+    path = config("sail_project_path") .. string.sub(position.path, string.len(vim.loop.cwd() or "") + 1)
+  end
 
-    local command = vim.tbl_flatten({
-        config('pest_cmd'),
-        path,
-        "--log-junit=" .. results_path,
+  local command = vim.tbl_flatten({
+    config("pest_cmd"),
+    path,
+    "--log-junit=" .. results_path,
+  })
+
+  if position.type == "test" then
+    command = vim.tbl_flatten({
+      command,
+      "--filter",
+      position.name,
     })
-
-    if position.type == "test" then
-        command = vim.tbl_flatten({
-            command,
-            "--filter",
-            position.name,
-        })
-    else
-        if config('is_parallel') then
-            command = vim.tbl_flatten({
-                command,
-                "--parallel",
-                "--processes=" .. config('parallel'),
-            })
-        end
+  else
+    if config("is_parallel") then
+      command = vim.tbl_flatten({
+        command,
+        "--parallel",
+        "--processes=" .. config("parallel"),
+      })
     end
+  end
 
+  if config("compact") == true then
+    info("Using compact output")
+    command = vim.tbl_flatten({
+      command,
+      "--compact",
+    })
+  end
 
-    if config('compact') == true then
-        info("Using compact output")
-        command = vim.tbl_flatten({
-            command,
-            "--compact",
-        })
-    end
+  debug("Command:", command)
 
-    debug("Command:", command)
-
-    return {
-        command = command,
-        context = {
-            results_path = results_path,
-        },
-    }
+  return {
+    command = command,
+    context = {
+      results_path = results_path,
+    },
+  }
 end
 
 ---@async
@@ -162,38 +166,38 @@ end
 ---@param tree neotest.Tree
 ---@return neotest.Result[]
 function NeotestAdapter.results(test, result, tree)
-    local output_file = test.context.results_path
+  local output_file = test.context.results_path
 
-    local ok, data = pcall(lib.files.read, output_file)
-    if not ok then
-        error("No test output file found! Should have been at: " .. output_file)
-        logger.error("No test output file found:", output_file)
-        return {}
-    end
+  local ok, data = pcall(lib.files.read, output_file)
+  if not ok then
+    error("No test output file found! Should have been at: " .. output_file)
+    logger.error("No test output file found:", output_file)
+    return {}
+  end
 
-    local ok, parsed_data = pcall(lib.xml.parse, data)
-    if not ok then
-        error("Failed to parse test output!")
-        logger.error("Failed to parse test output:", output_file)
-        return {}
-    end
+  local ok, parsed_data = pcall(lib.xml.parse, data)
+  if not ok then
+    error("Failed to parse test output!")
+    logger.error("Failed to parse test output:", output_file)
+    return {}
+  end
 
-    local ok, results = pcall(utils.get_test_results, parsed_data, output_file)
-    if not ok then
-        error("Could not get test results!")
-        logger.error("Could not get test results", output_file)
-        return {}
-    end
+  local ok, results = pcall(utils.get_test_results, parsed_data, output_file)
+  if not ok then
+    error("Could not get test results!")
+    logger.error("Could not get test results", output_file)
+    return {}
+  end
 
-    return results
+  return results
 end
 
 setmetatable(NeotestAdapter, {
-    __call = function(_, opts)
-        config.merge(opts or {})
+  __call = function(_, opts)
+    config.merge(opts or {})
 
-        return NeotestAdapter
-    end,
+    return NeotestAdapter
+  end,
 })
 
 return NeotestAdapter
--- lua/neotest-pest/utils.lua
+++ lua/neotest-pest/utils.lua
@@ -7,17 +7,19 @@ local separator = "::"
 ---@param position neotest.Position The position to return an ID for
 ---@return string
 M.make_test_id = function(position)
-    -- Treesitter ID needs to look like 'tests/Unit/ColsHelperTest.php::it returns the proper format'
-    -- which means it should include position.path. However, as of PHPUnit 10, position.path
-    -- includes the root directory of the project, which breaks the ID matching.
-    -- As such, we need to remove the root directory from the path.
-    local path = string.sub(position.path, string.len(vim.loop.cwd()) + 2)
+  -- Treesitter ID needs to look like 'tests/Unit/ColsHelperTest.php::it returns the proper format'
+  -- which means it should include position.path. However, as of PHPUnit 10, position.path
+  -- includes the root directory of the project, which breaks the ID matching.
+  -- As such, we need to remove the root directory from the path.
+  local path = string.sub(position.path, string.len(vim.loop.cwd()) + 2)
 
-    local id = path .. separator .. position.name
-    logger.debug("Path to test file:", { position.path })
-    logger.debug("Treesitter id:", { id })
+  -- Remover o trecho ' with data set "..."' se existir
+  local name = position.name:gsub(' with data set ".+"', "")
 
-    return id
+  local id = path .. separator .. name
+  logger.debug("Normalized Treesitter id:", { id })
+
+  return id
 end
 
 ---Recursively iterate through a deeply nested table to obtain specified keys
@@ -25,38 +27,54 @@ end
 ---@param key string
 ---@param output_table table
 ---@return table
-local function iterate_key(data_table, key, output_table)
-    if type(data_table) == "table" then
-        for k, v in pairs(data_table) do
-            if key == k then
-                table.insert(output_table, v)
-            end
-            iterate_key(v, key, output_table)
-        end
-    end
+local function iterate_key_recursive(data_table, key, output_table)
+  output_table = output_table or {}
+
+  if type(data_table) ~= "table" then
     return output_table
+  end
+
+  for k, v in pairs(data_table) do
+    if k == key then
+      if type(v) == "table" then
+        if v[1] then
+          -- É uma lista, adicionar todos os elementos
+          for _, item in ipairs(v) do
+            table.insert(output_table, item)
+          end
+        else
+          -- Único item
+          table.insert(output_table, v)
+        end
+      end
+    elseif type(v) == "table" then
+      iterate_key_recursive(v, key, output_table)
+    end
+  end
+
+  return output_table
 end
 
 ---Extract the failure messages from the tests
 ---@param tests table,
 ---@return boolean,table,table
 local function errors_or_fails(tests)
-    local failed = false
-    local errors = {}
-    local fails = {}
+  local failed = false
+  local errors = {}
+  local fails = {}
 
-    iterate_key(tests, "error", errors)
-    iterate_key(tests, "failure", fails)
+  iterate_key_recursive(tests, "error", errors)
+  iterate_key_recursive(tests, "failure", fails)
 
-    if #errors > 0 or #fails > 0 then
-        failed = true
-    end
+  if #errors > 0 or #fails > 0 then
+    failed = true
+  end
 
-    return failed, errors, fails
+  return failed, errors, fails
 end
 
 local function make_short_output(test_attr, status)
-    return string.upper(status) .. " | " .. test_attr.name
+  return string.upper(status) .. " | " .. test_attr.name
 end
 
 ---Make the outputs for a given test
@@ -64,54 +82,63 @@ end
 ---@param output_file string
 ---@return string, table
 local function make_outputs(test, output_file)
-    logger.debug("Pre-output test:", test)
-    local test_attr = test["_attr"] or test[1]["_attr"]
-    local name = string.gsub(test_attr.name, "^it (.*)", "%1")
+  logger.debug("Pre-output test:", test)
+  local test_attr = test["_attr"] or (test[1] and test[1]["_attr"])
+  local name = string.gsub(test_attr.name, "^it (.*)", "%1")
 
-    -- Difference to neotest-phpunit as of PHPUnit 10:
-    -- Pest's test IDs are in the format "path/to/test/file::test name"
-    local test_id = string.gsub(test_attr.file, "(.*)::(.*)", "%1") .. separator .. name
-    logger.debug("Pest id:", { test_id })
+  -- Pest's test IDs format: "path/to/test/file::test name"
+  local test_id = string.gsub(test_attr.file, "(.*)::(.*)", "%1") .. "::" .. name
+  logger.debug("Pest id:", { test_id })
 
-    local test_output = {
-        status = "passed",
-        short = make_short_output(test_attr, "passed"),
-        output_file = output_file,
-    }
+  local test_output = {
+    status = "passed",
+    short = make_short_output(test_attr, "passed"),
+    output_file = output_file,
+  }
 
-    local test_failed, errors, fails = errors_or_fails(test)
+  local test_failed, errors, fails = errors_or_fails(test)
 
-    if test_failed then
-        logger.debug("test_failed:", { test_failed, errors, fails })
-        test_output.status = "failed"
+  if test_failed then
+    logger.debug("test_failed:", { test_failed, errors, fails })
+    test_output.status = "failed"
 
-        if #errors > 0 then
-            local message = errors[1][1]
-            test_output.short = make_short_output(test_attr, "error") .. "\n\n" .. message
-            test_output.errors = {
-                {
-                    message = message
-                },
-            }
-        elseif #fails > 0 then
-            local message = fails[1][1]
-            test_output.short = make_short_output(test_attr, "failed") .. "\n\n" .. message
-            test_output.errors = {
-                {
-                    message = message
-                }
-            }
-        end
+    if #errors > 0 then
+      local message = errors[1]
+      if type(message) == "table" and type(message[1]) == "string" then
+        message = message[1]
+      elseif type(message) ~= "string" then
+        message = tostring(message) or ""
+      end
+      test_output.short = make_short_output(test_attr, "error") .. "\n\n" .. message
+      test_output.errors = {
+        {
+          message = message,
+        },
+      }
+    elseif #fails > 0 then
+      local message = fails[1]
+      if type(message) == "table" and type(message[1]) == "string" then
+        message = message[1]
+      elseif type(message) ~= "string" then
+        message = tostring(message) or ""
+      end
+      test_output.short = make_short_output(test_attr, "failed") .. "\n\n" .. message
+      test_output.errors = {
+        {
+          message = message,
+        },
+      }
     end
+  end
 
-    if test['skipped'] then
-        test_output.status = "skipped"
-        test_output.short = make_short_output(test_attr, "skipped")
-    end
+  if test["skipped"] then
+    test_output.status = "skipped"
+    test_output.short = make_short_output(test_attr, "skipped")
+  end
 
-    logger.debug("test_output:", test_output)
+  logger.debug("test_output:", test_output)
 
-    return test_id, test_output
+  return test_id, test_output
 end
 
 ---Iterate through test results and create a table of test IDs and outputs
@@ -120,15 +147,18 @@ end
 ---@param output_table table
 ---@return table
 local function iterate_test_outputs(tests, output_file, output_table)
-    for i = 1, #tests, 1 do
-        if #tests[i] == 0 then
-            local test_id, test_output = make_outputs(tests[i], output_file)
-            output_table[test_id] = test_output
-        else
-            iterate_test_outputs(tests[i], output_file, output_table)
-        end
+  output_table = output_table or {}
+
+  for i = 1, #tests, 1 do
+    if #tests[i] == 0 then
+      local test_id, test_output = make_outputs(tests[i], output_file)
+      output_table[test_id] = test_output
+    else
+      iterate_test_outputs(tests[i], output_file, output_table)
     end
-    return output_table
+  end
+
+  return output_table
 end
 
 ---Get the test results from the parsed xml
@@ -136,8 +166,8 @@ end
 ---@param output_file string
 ---@return neotest.Result[]
 M.get_test_results = function(parsed_xml_output, output_file)
-    local tests = iterate_key(parsed_xml_output, "testcase", {})
-    return iterate_test_outputs(tests, output_file, {})
+  local tests = iterate_key_recursive(parsed_xml_output, "testcase", {})
+  return iterate_test_outputs(tests, output_file, {})
 end
 
 return M
